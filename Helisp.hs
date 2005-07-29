d4587b9cc814f8ccd83bf113f75a99adc4098013
Jason Dagit <dagit@codersbase.com>
2005-07-29 04:51:34 +0000

    changed main to just parse, evaluation is not done

--- Helisp.hs
+++ Helisp.hs
@@ -3,9 +3,16 @@ module Main where
 import Parser
 import Eval
 
+-- this version of main will do the evaluation
+{- 
 main = do { c <- getContents;
             asts <- return (parseAst c);
             r <- return (map (eval symbolTable) asts);
             mapM print r }
+-}
+-- This version is for just parsing, output should be equivalent to input
+main = do { c <- getContents;
+            asts <- return (parseAst c);
+            mapM print asts }
 
 --main = getContents >>= (map print) . ((map (eval symbolTable)) . parseAst)
